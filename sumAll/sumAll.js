8a6d65a91ebe23abe1e2792c5ab7a0ed083135e9
chysomm62 <68497765+chysomm62@users.noreply.github.com>
2021-06-06 21:42:21 +0100

    sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,37 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+    let sum = 0;
+    for(i=a;i<=b;i++){
+            sum += i;
+    }const typea = typeof(a);
+    const typeb = typeof(b);
+
+    if ((typea === NaN) || (typeb === NaN) || (Array.isArray(typea)) || (Array.isArray(typeb))){
+        return "ERROR";
+    }
+    else if (typea === 'number' && typeb === 'number'){
+        if((a > 0) && (b > 0)){
+            
+            return sum; 
+            
+        }
+        else if ((a < 0) || (b < 0)){
+            return "ERROR";
+        }
+        else if (a > b){
+            const changer = a;
+            a = b;
+            b = changer;
+            return sum;
+        }
+        else {
+        }
+    }
+    else{
+        return "ERROR";
+    }
+     
 };
 
+sumAll(1,10);
 module.exports = sumAll;
