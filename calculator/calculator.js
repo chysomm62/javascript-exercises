b49df4d0c9cc945544a93e4993c209aca8dd8928
chysomm62 <68497765+chysomm62@users.noreply.github.com>
2021-06-08 10:44:53 +0100

    calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (i=0;i<array.length;i++){
+    sum = sum + array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum = 1;
+  for(i=0;i<array.length;i++){
+    sum = sum * array[i];
+  }
+  return sum
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0){
+    return 1;
+  }
+  else if(num < 0){
+    return NaN;
+  }
+  else{
+    return num * factorial(num - 1);
+    }
 };
 
 module.exports = {
