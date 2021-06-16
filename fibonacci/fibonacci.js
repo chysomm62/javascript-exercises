7e31382715554ebfd4e243d0c17aff46a68da9b0
chysomm62 <68497765+chysomm62@users.noreply.github.com>
2021-06-16 15:18:06 +0100

    din

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if(input < 0){
+        return "OOPS";
+    }
+    if(input === 0){
+        return 0;
+    }
+    let a = 1;
+    let b = 0; 
+    for(let i=1;i<input;i++){
+        const temp = a;
+        console.log(temp);
+        a = a + b;
+        b = temp;
+    }
+    return a;
 };
 
 module.exports = fibonacci;
